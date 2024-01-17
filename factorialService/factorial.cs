2dddeb45237f21af5cdeb539e399d860751f6137
Tia Self <tiamarie.self@bsu.edu>
2024-01-17 11:01:37 -0500

    add getFactorial method

--- factorialService/factorial.cs
+++ factorialService/factorial.cs
@@ -1,6 +1,14 @@
 ﻿namespace factorialService;
 
-public class factorial
-{
+public class factorial {
+
+    public static int getFactorial(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        else {
+            return n * getFactorial(n-1); 
+        }
+    }
 
 }
