da42ff571289c933789ac1be6cf57279f927ebd9
Tia Self <tiamarie.self@bsu.edu>
2024-01-23 12:35:07 -0500

    add missing semicolons

--- factorialService.Tests/factorialTests.cs
+++ factorialService.Tests/factorialTests.cs
@@ -5,17 +5,17 @@ public class factorialTests
 {
     [Fact]
     public void testFactorialValue_0(){
-        Assert.Equal(1,Factorial.getFactorial(0))
+        Assert.Equal(1,Factorial.getFactorial(0));
     }
 
     [Fact]
     public void testFactorialValue_5(){
-        Assert.Equal(120,Factorial.getFactorial(5))
+        Assert.Equal(120,Factorial.getFactorial(5));
     }
 
     [Fact]
     public void testFactorialValue_fail(){
-        Assert.NotEqual(100,Factorial.getFactorial(5))
+        Assert.NotEqual(100,Factorial.getFactorial(5));
     }
 
 }
